e2f698754e58f5b59f8d96dbfe3e61ca1cb7cf26
Pavel Rafeev <rafeev.pavel@gmail.com>
2016-10-12 19:25:22 +0200

    add less

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	connect = require('gulp-connect'),
 	livereload = require('gulp-livereload'),
-	rigger = require('gulp-rigger');
+	rigger = require('gulp-rigger'),
+    path = require('path'),
+    less = require('gulp-less');
 
 
 gulp.task('connect', function() {
@@ -22,7 +24,11 @@ gulp.task('html', function () {
     .pipe(connect.reload());
 });
 
-
+gulp.task('less', function () {
+  return gulp.src('less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('css'));
+});
 
 gulp.task('css', function() {
 return gulp.src('css/*.css')
@@ -34,7 +40,9 @@ return gulp.src('css/*.css')
 
 gulp.task('watch', function() {
 	gulp.watch('css/*.css', ['css'])
+    gulp.watch('less/*.less', ['less']);
 	gulp.watch('index.html', ['html']);
+
 });
 
-gulp.task('default', ['connect','html','css', 'watch'])
+gulp.task('default', ['connect','html','css', 'less', 'watch'])
